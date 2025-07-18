fe1000f0751af4d78bfb7b1b2db65841abe7ebba
Omer Cem Ozdemir <109981807+OmerCemOzdemir@users.noreply.github.com>
2025-07-18 01:17:59 +0100

    QuickFix: Door

--- Assets/Scripts/PlatformerScripts/Door.cs
+++ Assets/Scripts/PlatformerScripts/Door.cs
@@ -32,6 +32,7 @@ public class Door : MonoBehaviour
             PlatformerManager.onDoorEnter += DoorOpenAnimPlay;
             PlatformerManager.onDoorExit += DoorCloseAnimPlay;
         }
+
     }
 
     private void OnDisable()
