c4a7957eb6a0d12f29d250898c4d5929a3979e22
Luise Rivaldo <58327615+luiserivaldo@users.noreply.github.com>
2025-08-07 00:21:50 +0700

    linked other scenes to accomodate new intro cutscene
    most scenes are hardcoded to proceed to a specific scene index on trigger (sign next level, clicker exit button, main menu start)

--- Assets/Scripts/Utility/MainMenu.cs
+++ Assets/Scripts/Utility/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         onPlaySFX?.Invoke(SFX.Generic);
-        GameManager.Instance.NextLevel(1);
+        GameManager.Instance.NextLevel(7);
     }
 
     public void QuitGame()
